3a90b886ceb283a3cf8100d5d46ed594d92345d2
dhruvinparikh93 <imsilent93@gmail.com>
2020-06-28 23:50:00 -0400

    add correct server url

--- client/src/App.js
+++ client/src/App.js
@@ -12,14 +12,14 @@ class App extends Component {
       .catch(err => console.log(err));
   }
   callApi = async () => {
-    const response = await fetch('/users');
+    const response = await fetch('http://localhost:5000/users');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
     return body;
   };
   handleSubmit = async e => {
     e.preventDefault();
-    const response = await fetch('/device', {
+    const response = await fetch('http://localhost:5000/device', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
